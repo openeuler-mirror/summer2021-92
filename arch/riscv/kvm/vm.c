36f2dc3ad81ab6d6253915442cd3949aeecaeb91
Yifei Jiang <jiangyifei@huawei.com>
2020-09-07 10:19:48 +0800

    riscv/kvm: implement kvm_vm_ioctl_get_dirty_log
    euleros inclusion
    category: feature
    bugzilla: NA
    CVE: NA
    
    Implement the interface kvm_vm_ioctl_get_dirty_log for getting and
    clearing the log of dirty pages in a slot.
    
    Link: https://gitee.com/openeuler/kernel/issues/I1SWY2
    Signed-off-by: Yifei Jiang <jiangyifei@huawei.com>
    Signed-off-by: Yipeng Yin <yinyipeng1@huawei.com>
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- arch/riscv/kvm/vm.c
+++ arch/riscv/kvm/vm.c
@@ -12,10 +12,39 @@
 #include <linux/uaccess.h>
 #include <linux/kvm_host.h>
 
+/**
+ * kvm_vm_ioctl_get_dirty_log - get and clear the log of dirty pages in a slot
+ * @kvm: kvm instance
+ * @log: slot id and address to which we copy the log
+ *
+ * Steps 1-4 below provide general overview of dirty page logging. See
+ * kvm_get_dirty_log_protect() function description for additional details.
+ *
+ * We call kvm_get_dirty_log_protect() to handle steps 1-3, upon return we
+ * always flush the TLB (step 4) even if previous step failed  and the dirty
+ * bitmap may be corrupt. Regardless of previous outcome the KVM logging API
+ * does not preclude user space subsequent dirty log read. Flushing TLB ensures
+ * writes will be marked dirty for next log read.
+ *
+ *   1. Take a snapshot of the bit and clear it if needed.
+ *   2. Write protect the corresponding page.
+ *   3. Copy the snapshot to the userspace.
+ *   4. Flush TLB's if needed.
+ */
 int kvm_vm_ioctl_get_dirty_log(struct kvm *kvm, struct kvm_dirty_log *log)
 {
-	/* TODO: To be added later. */
-	return -ENOTSUPP;
+	bool flush = false;
+	int r;
+
+	mutex_lock(&kvm->slots_lock);
+
+	r = kvm_get_dirty_log_protect(kvm, log, &flush);
+
+	if (flush)
+		kvm_flush_remote_tlbs(kvm);
+
+	mutex_unlock(&kvm->slots_lock);
+	return r;
 }
 
 int kvm_arch_init_vm(struct kvm *kvm, unsigned long type)
